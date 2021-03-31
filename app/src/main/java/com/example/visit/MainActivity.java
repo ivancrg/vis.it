617913ac72db0a8ebd1d40833c3a666697f8f1c2
lergahana <46451875+lergahana@users.noreply.github.com>
2021-03-31 13:29:59 +0200

    Closes #3; Login/Register Interface

--- app/src/main/java/com/example/visit/MainActivity.java
+++ app/src/main/java/com/example/visit/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.visit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.visit.ui.login.LoginFragment;
+import com.example.visit.ui.login.RegisterFragment;
+
+public class MainActivity extends AppCompatActivity implements CallbackFragment{
+
+    Fragment fragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addFragment();
+    }
+
+    public void addFragment(){
+        LoginFragment fragment = new LoginFragment();
+        fragment.setCallbackFragment(this);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentLogin, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void replaceFragment(){
+        fragment = new RegisterFragment();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.fragmentRegister, fragment);
+        fragmentTransaction.commit();
     }
 
 
+    @Override
+    public void changeFragment() {
+        replaceFragment();
+    }
 }
---
+++ app/src/main/java/com/example/visit/ui/login/RegisterViewModel.java
@@ -0,0 +1,7 @@
+package com.example.visit.ui.login;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
