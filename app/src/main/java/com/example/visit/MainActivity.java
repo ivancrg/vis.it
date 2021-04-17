64f5dc2f1ce1288c3b9c8fb424054c0e8ec55b06
irubi516 <ivan.rubinic@outlook.com>
2021-04-17 22:27:24 +0200

    Fixes #8; Bug when relaunching the app (null pointer)
    Fixed navigation drawer bug. When relaunching, app was crashing due to null pointer exception which was caused by lines that change navigation header TextViews in UserInterfaceFragment.java (onCreateView and setLoggedUser).

--- app/src/main/java/com/example/visit/MainActivity.java
+++ app/src/main/java/com/example/visit/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.visit;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,7 +14,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
     private DrawerLayout drawerLayout;
@@ -30,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         //setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.main_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.navigation_drawer_open,
@@ -41,6 +49,44 @@ public class MainActivity extends AppCompatActivity {
         addFragment();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        // Here you can uncomment lines to enable redirection according to clicks from navigation drawer
+        // and delete return false to make selection in navigation drawer visible
+
+        if (itemId == R.id.nav_explore) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NEWS_EXPLORE_FRAGMENT_CLASS_NAME());
+            return false;
+        } else if (itemId == R.id.nav_add_plan) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ADD_PLAN_FRAGMENT_CLASS_NAME());
+            return false;
+        } else if (itemId == R.id.nav_travelling) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TRAVELLING_FRAGMENT_CLASS_NAME());
+            return false;
+        } else if (itemId == R.id.nav_on_location) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ON_LOCATION_FRAGMENT_CLASS_NAME());
+            return false;
+        } else if (itemId == R.id.nav_account) {
+            if (LoggedUser.getIsLoggedIn()) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserInterfaceFragment());
+            } else {
+                Toast.makeText(this, "You are currently not logged in.", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        } else if (itemId == R.id.nav_settings) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SETTINGS_FRAGMENT_CLASS_NAME());
+            return false;
+        } else if (itemId == R.id.nav_share) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SHARE_FRAGMENT_CLASS_NAME());
+        } else if (itemId == R.id.nav_contact_support) {
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SUPPORT_FRAGMENT_CLASS_NAME());
+        }
+
+        return true;
+    }
+
     public void addFragment() {
         LoginFragment fragment = new LoginFragment();
         fragmentManager = getSupportFragmentManager();
@@ -50,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed(){
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        } else{
+        } else {
             super.onBackPressed();
         }
     }
