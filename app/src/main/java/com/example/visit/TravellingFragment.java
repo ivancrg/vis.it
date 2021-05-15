ee39f4b0072e04574bf10d548f9fff4ca4817b50
irubi516 <ivan.rubinic@outlook.com>
2021-05-16 00:34:38 +0200

    Issue #61; MapView now showing pre-coded location
    TODO: check granted location permissions, show current location and distance to the destination. Also, destination coordinates need to be forwarded to this fragment somehow.

--- app/src/main/java/com/example/visit/TravellingFragment.java
+++ app/src/main/java/com/example/visit/TravellingFragment.java
@@ -1,10 +1,8 @@
 package com.example.visit;
 
-import android.media.Image;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +10,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.MapView;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 
-public class TravellingFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class TravellingFragment extends Fragment implements OnMapReadyCallback {
+    // TODO: move to local.properties (so it becomes a hidden variable)
+    private static final String MAPVIEW_BUNDLE_KEY = "AIzaSyArpz0rGtzrmJHGxEz-FB71GhnasO2wz0I";
+    // Integer permission descriptors?
+    private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
+
+    private MapView mapView;
+
     public TravellingFragment() {
         // Required empty public constructor
     }
@@ -29,12 +42,13 @@ public class TravellingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_travelling, container, false);
-
-        MapView mapView = (MapView) view.findViewById(R.id.travellingFragmentMapView);
+        mapView = (MapView) view.findViewById(R.id.travellingFragmentMapView);
         Button arrivedButton = (Button) view.findViewById(R.id.travellingFragmentArrivedButton);
         ImageView weatherIcon = (ImageView) view.findViewById(R.id.travellingFragmentWeatherIcon);
         ImageView musicIcon = (ImageView) view.findViewById(R.id.travellingFragmentMusicIcon);
         ImageView clockIcon = (ImageView) view.findViewById(R.id.travellingFragmentClockIcon);
+
+        initGoogleMap(savedInstanceState);
 
         arrivedButton.setOnClickListener(view1 -> {
             Toast.makeText(getContext(), "TEMP_TEXT_NOT_IMPLEMENTED Congrats!", Toast.LENGTH_SHORT).show();
@@ -53,5 +67,75 @@ public class TravellingFragment extends Fragment {
         });
 
         return view;
+    }
+
+    // initGoogleMap used just as a way to clean up the code in onCreate method a little bit
+    private void initGoogleMap(Bundle savedInstanceState) {
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // TODO show destination location instead of Rijeka
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(45.34306, 14.40917))
+                .title("Trip destination"));
+
+        // TODO show current location
+    }
+
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
